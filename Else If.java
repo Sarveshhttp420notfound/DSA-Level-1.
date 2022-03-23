90c8266fb62286ae276efffae4fb0177796ea315
Sarveshhttp420notfound <87657797+Sarveshhttp420notfound@users.noreply.github.com>
2022-03-23 17:00:52 +0530

    Add files via upload

---
+++ Else If.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class main{
+    public static void main(String[]args){
+        int age=25;
+        if(age==25){
+            System.out.println("Married");
+        }
+        else if(age>25){
+            System.out.println("CNF married");
+        }
+        else{
+            System.out.println("not married");
+        }
+    }
+}
