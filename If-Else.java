f13c0d5bccab0f07818aa5dab173fbc719589d05
Sarveshhttp420notfound <87657797+Sarveshhttp420notfound@users.noreply.github.com>
2022-03-23 15:29:18 +0530

    Add files via upload

---
+++ If-Else.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        int age=100;
+        if(age==100){
+            System.out.println("Very old");
+        }
+        else{
+            
+            System.out.println("not old");
+        }
+    }
+}
